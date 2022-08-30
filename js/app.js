e21dda8b40e93626df87850cee0421bbe4c20812
ManasSarker <manas.sarker.moon@gmail.com>
2022-08-30 23:53:47 +0600

    add api link

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const loadPhones = async (searchText, dataLimit) => {
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayPhones(data.data, dataLimit);
+};
