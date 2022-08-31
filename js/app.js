35d2b7fbe65996fd8e23140670f0c0fc910faf97
ManasSarker <manas.sarker.moon@gmail.com>
2022-08-31 13:01:48 +0600

    adding the search function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-const loadPhones = async () => {
-  const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+const loadPhones = async (search) => {
+  const url = `https://openapi.programming-hero.com/api/phones?search=${search}`;
   const res = await fetch(url);
   const data = await res.json();
   displayPhones(data.data);
@@ -30,15 +30,13 @@ const displayPhones = (phones) => {
 };
 
 loadPhones();
-// const displayPhones = (phones, dataLimit) => {
-//   const phonesContainer = document.getElementById("phones-container");
-//   phonesContainer.textContent = "";
-//   // display 10 phones only
-//   const showAll = document.getElementById("show-all");
-//   if (dataLimit && phones.length > 10) {
-//     phones = phones.slice(0, 10);
-//     showAll.classList.remove("d-none");
-//   } else {
-//     showAll.classList.add("d-none");
-//   }
-// };
+
+/*  Searching Function  */
+
+document.getElementById("btn-search").addEventListener("click", function () {
+  const searchFiled = document.getElementById("search-field");
+
+  const searchText = searchFiled.value;
+
+  loadPhones(searchText);
+});
